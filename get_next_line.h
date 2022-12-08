598409da03e1d54cbc90ff18ff6232c5dcc20bce
Yousra <110408013+yousrachakib@users.noreply.github.com>
2022-12-08 15:26:18 +0100

    last changes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 17:32:16 by yochakib          #+#    #+#             */
-/*   Updated: 2022/12/04 17:03:01 by yochakib         ###   ########.fr       */
+/*   Updated: 2022/12/08 15:21:54 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,18 @@
 # include <unistd.h>
 # include <fcntl.h>
 
+# ifndef BUFFER_SIZE
+#  define  BUFFER_SIZE 5
+# endif
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *s);
+int		detect_newline(char *s);
+char	*ft_line(char *res);
+char	*new_res(char *res);
+char	*ft_read(int fd, char *res);
+char	*get_next_line(int fd);
 
 #endif
